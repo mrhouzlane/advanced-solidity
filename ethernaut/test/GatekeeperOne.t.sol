a0328a2910c8618f343daa0417c0184af7112e80
Mehdi <rhouzm@gmail.com>
2024-01-29 23:19:04 +0100

    feat: Add HUFF files

---
+++ ethernaut/test/GatekeeperOne.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import "forge-std/test.sol";
+import {GatekeeperOne} from "../src/GatekeeperOne.sol";
+
+contract GatekeeperOneTest is Test {
+    GatekeeperOne public gatekeeper;
+
+    function setUp() public {
+        gatekeeper = new GatekeeperOne();
+    }
+
+    function testEnterGate() public {
+        // gateOne : make sure call is from a contract 
+        // gateTwo : make sure gasleft() is a multiple of 8191
+        // gateThree : make sure the last 2 bytes of the gateKey are equal to the first 2 bytes of the tx.origin address
+
+        
+
+    }
+}
