da3cd0fcba26df50c933d589c6fd9f850f58398b
mrhouzlane <rhouzm@gmail.com>
2024-04-05 17:25:56 +0000

    ok

---
+++ ctfs/Mapping/test/MappingChallenge.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {MappingChallenge} from "../src/MappingChallenge.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract MappingChallengeTest is Test {
+
+    MappingChallenge public mappingChallenge; 
+
+    function setUp() public {
+        
+    }
+
+
+
+    
+   
+}
